f122da0df83d34485de1a58a555ce6061f180a3b
RUGIRA NDUNGUTSE Egide <rugiraegide9@gmail.com>
2023-11-27 20:21:25 +0200

    ghhg

---
+++ login.php
@@ -0,0 +1,90 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>login</title>
+    <link rel="stylesheet" href="login.css">
+</head>
+<body>
+    <form action="" method="post">
+        <h2>RUGIRA INDUSTRIES</h2>
+        <input type="text" placeholder="Username">
+        <input type="password" placeholder="Password">
+        <a href="" id="i7">Forgot password?</a>
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="style.css">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+    <title>LOGIN</title>
+</head>
+
+<body>
+    <!-- Menu  -->
+    <div class="menu">
+
+        <div class="logo">
+            <h1>My Brand</h1>
+        </div>
+
+        <div class="links">
+
+            <ul>
+            <li><a href=""><i class="fa fa-home" aria-hidden="true"></i> Home</a></li>
+                <li><a href=""><i class="fa fa-phone" aria-hidden="true"></i> Contact</a></li>
+                <li><a href=""><i class="fa fa-rss" aria-hidden="true"></i> Blog</a></li>
+                <li><a href=""><i class="fa fa-sign-in" aria-hidden="true"></i> Login</a></li>
+
+            </ul>
+            
+        </div>
+
+    </div>
+    <div>
+        
+    </div>
+    
+    </div>
+    <div class="bod">
+    
+        <h3><i class="fa fa-address-card-o" aria-hidden="true"></i> LOGIN</h3>
+        
+    </div>
+    <div class="formcontainer">
+        <div class="formelements">
+        <input type="email" placeholder="email"><br>
+        <input type="number" placeholder="phone"><br>
+        <button>login</button>
+        </div>
+       
+</div>
+        <div class="footer">
+        <div class="left-footer">
+            <ul>
+                <li><a href="http://"><i class="fa fa-facebook-official" aria-hidden="true"></i> Facebook</a> </li>
+                <li><a href="http://"><i class="fa fa-instagram" aria-hidden="true"></i> Instagram</a> </li>
+                <li><a href="http://"><i class="fa fa-youtube-play" aria-hidden="true"></i> Youtube</a> </li>
+            </ul>
+        </div>
+        <div class="right-footer">
+            <span>RugiraEgide2023</span>
+        </div>
+
+    </div>
+
+</body>
+</html>
+        <div class="buttons">
+            <button type="login" name="login">Login</button>
+            <h5>or</h5>
+            <a href="">Signup</a>
+        </div>
+    </form>
+</body>
+</html>
+<?p
